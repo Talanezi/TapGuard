2a67df18b147abfa0ec4ffeaf24549d0fd7d094d
Thamer Alanezi <talanezi@ucsd.edu>
2025-12-25 13:25:43 +0300

    Initial Commit

---
+++ TapGuard/TapGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapGuardApp.swift
+//  TapGuard
+//
+//  Created by Thamer Alanezi on 12/25/25.
+//
+
+import SwiftUI
+
+@main
+struct TapGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
